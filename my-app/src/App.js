6e84f7929cb095913966e860bdadd6f9c6e7317d
upluke <lukeforadream@gmail.com>
2020-06-08 21:50:55 -0700

    folders created

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
        <h1>v20-geckos-team-04</h1>
+       <h2>Components is created</h2>
+       <h2>Pages is created</h2>
     </div>
   );
 }
