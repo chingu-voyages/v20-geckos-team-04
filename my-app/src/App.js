93aaa87a277bdc01564ebae23e7dbda61fd0975a
Chelsea <chelsea.hy.tang@gmail.com>
2020-06-10 19:22:13 -0700

    created left navigation sidebar with routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+import Nav from './Nav';
+import Home from './pages/Home';
+import History from './pages/History';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-       <h1>v20-geckos-team-04</h1>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Nav className="sidenav" />
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/history" component={History} />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- my-app/src/pages/History.js
+++ my-app/src/pages/History.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../App.css';
 
-function Home() {
+function History() {
 	return (
 		<div>
-			<h1>Home Page</h1>
+			<h1>History Page</h1>
 		</div>
 	);
 }
 
-export default Home;
+export default History;
