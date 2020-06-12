f2027aa8e3ef70d71d2697dc52df217b967790f0
upluke <lukeforadream@gmail.com>
2020-06-11 20:54:19 -0700

    clearnOut

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import './App.css';
-import Nav from './Nav';
-import Home from './pages/Home';
-import History from './pages/History';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
 	return (
-		<Router>
 			<div className="App">
-				<Nav className="sidenav" />
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route path="/history" component={History} />
-				</Switch>
+				v20-geckos-team-04
 			</div>
-		</Router>
+	 
 	);
 }
 
