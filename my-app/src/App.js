bb93adfe4d39e9c69b5969c3ea523bc50e526a77
Chelsea <chelsea.hy.tang@gmail.com>
2020-06-12 15:14:21 -0700

    navbar ready for review

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
-
+import Nav from './Nav';
+import Home from './pages/Home';
+import History from './pages/History';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
 	return (
+		<Router>
 			<div className="App">
-				v20-geckos-team-04
+				<Nav className="sidenav" />
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/history" component={History} />
+				</Switch>
 			</div>
-	 
+		</Router>
 	);
 }
 
