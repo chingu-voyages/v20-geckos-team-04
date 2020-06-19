f0670ba8bb1fda622596dff28262eb1e15109c35
MobiusMan <andrewbuffington1@gmail.com>
2020-06-19 23:09:27 +0000

    Attempting to re-push all files and keep same directory places.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import './App.css';
-import Nav from './Nav';
+import NavLeft from './Nav';
 import Home from './pages/Home';
 import History from './pages/History';
+import TopMenu from './components/TopMenu';
+import Routes from './components/Routes';
+import Card from './components/Card';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
 	return (
-		<Router>
-			<div className="App">
-				<Nav className="sidenav" />
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route path="/history" component={History} />
-				</Switch>
-			</div>
-		</Router>
+        <div className="container-fluid">
+            <div class="row">
+                <div className="col-sm-2 border border-primary border-right-0 border-bottom-0 p-3">
+                    <h3>Geckos-team-04</h3>
+                </div>
+                    <TopMenu />
+            </div>
+            <div class="row">
+                    <Router>
+                        <NavLeft />
+                    </Router>
+                <div className="col-sm-10 p-3 bg-white overflow-auto">
+                    <Card />
+                </div>
+            </div>
+        </div>
 	);
 }
 
 export default App;
+
--- my-app/src/Nav.js
+++ my-app/src/Nav.js
@@ -1,31 +1,27 @@
 import React from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faFolder } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Nav() {
-	const navStyle = {
-		color: 'white',
-	};
-
-	return (
-		<nav>
-			<h3>Geckos-Team04</h3>
-			<ul className="nav-links">
-				<Link className="main-nav" style={navStyle} to="/">
-					<li>
-						<img src="https://img.icons8.com/android/24/000000/home.png" />
-						Home
+const Nav = () => (
+		<div className="col-2 border border-primary">		
+			<ul class="nav flex-column nav-pills">
+				<Link to="/">
+					<li className="nav-link p-3 mb-2 text-center left-link">
+						<FontAwesomeIcon icon={faHome} size="lg" className="mr-2" />
+							Home
 					</li>
 				</Link>
-				<Link className="main-nav" style={navStyle} to="/history">
-					<li>
-						<img src="https://img.icons8.com/ios/50/000000/opened-folder.png" />
-						History
+				<Link to="/history">
+					<li className="nav-link p-3 text-center left-link">
+						<FontAwesomeIcon icon={faFolder} size="lg" className="mr-2" />
+							History
 					</li>
 				</Link>
 			</ul>
-		</nav>
+		</div>
 	);
-}
 
 export default Nav;
